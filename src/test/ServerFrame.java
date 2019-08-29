71cec5bca8396125cdaeb2dbed7445dcf0b441ba
HongEunbeen <vvvv980@naver.com>
2019-08-29 09:28:41 +0900

    a

--- src/test/ServerFrame.java
+++ src/test/ServerFrame.java
@@ -190,6 +190,12 @@ public class ServerFrame extends JFrame {
 				while(true) {
 					socket = serverSocket.accept();//클라이언트가 접속할때까지 커서(스레드)가 대기한다.
 					chat_room.append("<" + socket.getLocalPort() +" : " + socket.getPort()+ "님이 접속하셨습니다.>\n");
+					System.out.println(socket.getInetAddress()+"의 "+socket.getPort()
+
+					+ "포트에" + socket.getLocalAddress()+"의 "+socket.getLocalPort()
+
+					+ "포트로 연결되었습니다.");
+
 					
 					Receiver receiver = new Receiver(socket);//Receiver를 이용해서 네트워크 소켓을 받아서 계속듣고 보내는 일을 한다.
 	                receiver.start();
